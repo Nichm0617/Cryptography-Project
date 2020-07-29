22bfe2e6d7429e4ada1d243afa1cebdae4e2f8d5
matacn <matacn@rpi.edu>
2020-07-29 17:48:23 -0400

    added hello

--- client.py
+++ client.py
@@ -1,23 +1,54 @@
 import socket
 import sys
+import time
+import random
+import json
+import gzip
+import pickle
 
 class Client:
-    def __init__(self):
-        self.srv_address = ('localhost', 12345)
+	def __init__(self):
+		self.srv_address = ('localhost', 12345)
 
-    def server_connect(self):
-        self.srv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.srv_sock.connect(self.srv_address)
-        print("Connected")
+	def server_connect(self):
+		self.srv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.srv_sock.connect(self.srv_address)
+		print("Connected")
 
-    def server_disconnect(self):
-    	self.srv_sock.close()
-    	print("Disconnected")
+	def server_disconnect(self):
+		self.srv_sock.close()
+		print("Disconnected")
+
+	def send_hello(self):
+		"""
+		Send the hello, this includes information needed for encryption
+		"""
+		pack = {'type': 'hello'}
+		pack['sym_key_type'] = 'des'
+		pack['key_exc_type'] = 'rsa'
+		pack['hash_type'] = 'sha1'
+		pack = json.dumps(pack).encode('utf-8')
+		self.srv_sock.sendall(pack)
+		print("Sent Hello")
+
+	def recive_hello(self):
+		"""
+		Recieve the hello from the server, including essential info
+		"""
+		pack = self.srv_sock.recv(20000)
+		pack = json.loads(pack.decode('utf-8'))
+		hash_type = pack['hash_type']
+		key_exec_type = pack['key_exc_type']
+		sym_key_type = pack['sym_key_type']
+		#now do something with this information here
+		print("Recived Hello")
 
 if __name__ == "__main__":
 	client = Client()
 	client.server_connect()
-	client.server_disconnect()
 	"""
 	Handshake Step
 	"""
+	client.send_hello()
+	client.recive_hello()
+	client.server_disconnect()
--- server.py
+++ server.py
@@ -1,30 +1,64 @@
 import socket
+import json
 import sys
 
 class Server:
-    def __init__(self):
-        self.srv_address = ('localhost', 12345)
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.bind(self.srv_address)
-        self.sock.listen(1)
-        print("SSL Server started at", self.srv_address)
+	def __init__(self):
+		self.srv_address = ('localhost', 12345)
+		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.sock.bind(self.srv_address)
+		self.sock.listen(1)
+		print("SSL Server started at", self.srv_address)
 
-    def client_connect(self):
-        """
-        Allows a client to connect to this server
-        """
-        while True:
-        	#Waits for the connection here
-            connection, client_address = self.sock.accept()
-            try:
-                print('client address:', client_address)
-                print('connection information:', connection)
+	def client_connect(self):
+		"""
+		Allows a client to connect to this server
+		"""
+		while True:
+			#Waits for the connection here
+			connection, client_address = self.sock.accept()
+			try:
+				print('client address:', client_address)
+				print('connection information:', connection)
+				"""
+				Handshake part
+				"""
+				# Exchange Hello part
+				self.recive_hello(connection)
+				self.send_hello(connection)
+			finally:
+				connection.close()
+				print("Disconected")
+				break
 
-            finally:
-                print("Disconected")
-                connection.close()
-                break
+	def send_hello(self, connection):
+		"""
+		Send the selected suit back to client
+		"""
+		pack = {'type': 'hello'}
+		pack['sym_key_type'] = self.sym_key_type
+		pack['key_exc_type'] = self.key_exc_type
+		pack['hash_type'] = self.hash_type
+		pack = json.dumps(pack).encode('utf-8')
+		connection.sendall(pack)
+		print("Sent Hello")
+
+	def recive_hello(self, connection):
+		"""
+		Recive a cipher suit from client for the hello
+		"""
+		pack = connection.recv(20000)
+		pack = json.loads(pack.decode('utf-8'))
+		if(pack['sym_key_type'] == 'des' and pack['key_exc_type'] == 'rsa' and pack['hash_type'] == 'sha1'):
+			self.sym_key_type = pack['sym_key_type']
+			self.key_exc_type = pack['key_exc_type']
+			self.hash_type = pack['hash_type']
+			print("Recived Hello")
+		else:
+			print("Incompatible Suite. Shutting Down")
+			print("Disconected")
+			connection.close()
 
 if __name__ == "__main__":
-    server = Server()
-    server.client_connect()
+	server = Server()
+	server.client_connect()
