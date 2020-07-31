c460bea51747c2bc7c35784564edf910145cb86d
matacn <matacn@rpi.edu>
2020-07-31 01:12:32 -0400

    hanshake implemented

--- client.py
+++ client.py
@@ -1,6 +1,9 @@
 import socket
 import json
 import sys
+import zlib
+import gzip
+import secrets
 
 class Client:
 	def __init__(self):
@@ -19,6 +22,7 @@ class Client:
 		"""
 		Send the hello, this includes information needed for encryption
 		"""
+		#may need to also send protocol version, session ID, compression method, and inital random numbers, though I am unsure what those would be
 		pack = {'type': 'hello'}
 		pack['sym_key_type'] = 'des'
 		pack['key_exc_type'] = 'rsa'
@@ -36,15 +40,122 @@ class Client:
 		hash_type = pack['hash_type']
 		key_exec_type = pack['key_exc_type']
 		sym_key_type = pack['sym_key_type']
-		#now do something with this information here
 		print("Recived Hello")
 
+	def send_public_keys(self):
+		"""
+		Generate public keys and send them to the server
+		"""
+
+		#generate keys here, the public keys used with RSA
+
+		pack = {'type': 'public_keys'}
+		pack['key_1_public'] = 'placeholder 1' #replace with keys
+		pack['key_2_public'] = 'placeholder 2' #replace with keys
+		pack = json.dumps(pack).encode('utf-8')
+		self.srv_sock.sendall(pack)
+		print("Sent Public Keys")
+
+	def recive_public_keys(self):
+		"""
+		Recive the servers public keys
+		"""
+		pack = self.srv_sock.recv(20000)
+		pack = json.loads(pack.decode('utf-8'))
+		self.server_public_key_1 = pack['key_1_public']
+		self.server_public_key_2 = pack['key_2_public']
+		print("Recived Public Keys From Server")
+
+	def send_symmetric_key(self):
+		"""
+		Encrypt a symmetric key and send it to the server
+		"""
+		key_length = 100 #not sure about what size the key should be, this will effect DES
+		self.sym_key = secrets.randbits(key_length) #this is a secure way of generating random bits
+
+		#encrypt the key here before sending with RSA
+
+		pack = {'type': 'symmetric_key'}
+		pack['key_symmetric'] = 'placeholder' #replace with encrypted key
+		pack = json.dumps(pack).encode('utf-8')
+		self.srv_sock.sendall(pack)
+		print("Sent Symmetric Key")
+
+	def confirm_hanshake(self):
+		"""
+		Receive confirmation from the server
+		"""
+		pack = self.srv_sock.recv(20000)
+		pack = json.loads(pack.decode('utf-8'))
+		print("Handshake complete")
+
+	def send_message(self, message):
+		"""
+		Compress the message, generate its hash, encrypt, and send to server
+		"""
+		message_bytes = message.encode()
+		compressed = gzip.compress(message_bytes)
+
+		#compute MAC of the compressed message here
+		#encrypt the compressed message here
+
+		pack = {'type': 'message'}
+		pack['message'] = message
+		pack['MAC'] = 'placeholder'
+
+		pack = json.dumps(pack).encode('utf-8')
+		self.srv_sock.sendall(pack)
+
+	def receive_message(self):
+		"""
+		receive a message from the server
+		"""
+		pack = self.srv_sock.recv(20000)
+		# server terminated connection
+		if pack.decode('ascii') == "":
+			sys.exit()
+		pack = json.loads(pack.decode('utf-8'))
+
+		encrypted = pack['message']
+
+		# decrypt here
+		# decompress here
+		# message = decompressed.decode('utf-8')
+		message = encrypted #remove this once it works
+
+		#verify MAC here
+
+		print("Received message from client:", message)
+		
 if __name__ == "__main__":
 	client = Client()
 	client.server_connect()
+
 	"""
 	Handshake Step
 	"""
+	#hello step
 	client.send_hello()
 	client.recive_hello()
+
+	#public key step
+	client.send_public_keys()
+	client.recive_public_keys()
+
+	#symmetric key step
+	client.send_symmetric_key()
+	client.confirm_hanshake()
+
+	"""
+	Banking Step
+	"""
+	#send message and wait for a respose, these messages can be a command for the server
+	message = ""
+	while True:
+		message = input("Enter a command: ")
+		if message == "exit":
+			break
+		client.send_message(message)
+		client.receive_message()
+
 	client.server_disconnect()
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import socket
 import json
+import gzip
 import sys
 
 class Server:
@@ -17,24 +18,46 @@ class Server:
 		while True:
 			#Waits for the connection here
 			connection, client_address = self.sock.accept()
-			try:
-				print('client address:', client_address)
-				print('connection information:', connection)
-				"""
-				Handshake part
-				"""
-				# Exchange Hello part
-				self.recive_hello(connection)
-				self.send_hello(connection)
-			finally:
-				connection.close()
-				print("Disconected")
-				break
+
+			print('client address:', client_address)
+			print('connection information:', connection)
+			"""
+			Handshake part
+			"""
+			# Exchange Hello part
+			self.recive_hello(connection)
+			self.send_hello(connection)
+
+			#public key exchange part
+			self.recive_public_keys(connection)
+			self.send_public_keys(connection)
+
+			#symmetric key step
+			self.recive_symmetric_key(connection)
+			self.send_confirmation(connection)
+
+			"""
+			Banking Step
+			"""
+			#This should later on be replaced with options for all the commands the client might send
+			message = ""
+			while True:
+				self.receive_message(connection)
+				message = input("Enter a command: ")
+				if message == "exit":
+					break
+				self.send_message(connection, message)
+
+
+			connection.close()
+			print("Disconected")
+			break
 
 	def send_hello(self, connection):
 		"""
 		Send the selected suit back to client
 		"""
+		#may need to also send protocol version, session ID, compression method, and inital random numbers
 		pack = {'type': 'hello'}
 		pack['sym_key_type'] = self.sym_key_type
 		pack['key_exc_type'] = self.key_exc_type
@@ -58,6 +81,88 @@ class Server:
 			print("Incompatible Suite. Shutting Down")
 			print("Disconected")
 			connection.close()
+
+	def send_public_keys(self, connection):
+		"""
+		Generate public keys and send them to the client
+		"""
+		#generate keys here
+		pack = {'type': 'public_keys'}
+		pack['key_1_public'] = 'placeholder 1' #replace with keys
+		pack['key_2_public'] = 'placeholder 2'
+		pack = json.dumps(pack).encode('utf-8')
+		connection.sendall(pack)
+		print("Sent Public Keys")
+
+	def recive_public_keys(self, connection):
+		"""
+		Recive public keys from the client
+		"""
+		pack = connection.recv(20000)
+		pack = json.loads(pack.decode('utf-8'))
+		self.client_public_key_1 = pack['key_1_public']
+		self.client_public_key_2 = pack['key_2_public']
+		print("Recived Public Keys From Client")
+
+	def recive_symmetric_key(self, connection):
+		"""
+		Recive encrypted symmetric key from client
+		"""
+		pack = connection.recv(20000)
+		pack = json.loads(pack.decode('utf-8'))
+		encrypted_symmetric_key = pack['key_symmetric']
+		#decrypt and save key here
+		print("Recived Symmetric Key")
+
+	def send_confirmation(self, connection):
+		"""
+		Send confirmation that handshake is complete
+		"""
+		pack = {'type': 'confirmation'}
+		pack = json.dumps(pack).encode('utf-8')
+		pack = connection.sendall(pack)
+		print("Handshake complete")
+
+	def send_message(self, conn, message):
+		"""
+		Compress the message, generate its hash, encrypt, and send to client
+		"""
+		message_bytes = message.encode()
+		compressed = gzip.compress(message_bytes)
+
+		#compute MAC of the compressed message here
+		#encrypt the compressed message here
+
+		pack = {'type': 'message'}
+		pack['message'] = message
+		pack['MAC'] = 'placeholder'
+
+		pack = json.dumps(pack).encode('utf-8')
+		conn.sendall(pack)
+
+	def receive_message(self, conn):
+		"""
+		receive a message from the client
+		"""
+		pack = conn.recv(20000)
+		# Client terminated connection
+		if pack.decode('ascii') == "":
+			print("Disconnected")
+			sys.exit()
+			
+		pack = json.loads(pack.decode('utf-8'))
+
+		encrypted = pack['message']
+
+		# decrypt here
+		# decompress here
+		# message = decompressed.decode('utf-8')
+		message = encrypted #remove this once it works
+
+		#verify MAC here
+
+		print("Received message from client:", message)
+
 
 if __name__ == "__main__":
 	server = Server()
